ee672a999cc94236772dd867d8cbd55509bbbe40
chenqingsong <chenqingsong@zhangyue.com>
2019-07-12 14:38:00 +0800

    编译优化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
+const path = require('path')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+const isProduction = true
+
 module.exports = {
+    lintOnSave: false,
+    productionSourceMap: false,
+    configureWebpack: config => {
+        if (isProduction) {
+            config.plugins.push(new CompressionWebpackPlugin({
+                    algorithm: 'gzip',
+                    test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+                    threshold: 10240,
+                    minRatio: 0.8
+                })
+            )
+        }
+    },
     devServer: {
         proxy: {
             '/console': {
